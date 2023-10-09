aacd5d37015d1eee34fe956466ffaa40083c2633
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-10-09 22:54:44 +0530

    38. Automapper implementation example1.

---
+++ NZWalk.API/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+
+namespace NZWalk.API.Mappings
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<UserDTO, UserDomain>()
+                .ForMember(x => x.Name, opt=> opt.MapFrom(x=> x.FullName))  // for non matching properties define mapping 
+                .ReverseMap(); // ReverseMap means UserDTO to UserDomain and also UserDomain to UserDTO
+        }
+
+        public class UserDTO
+        {
+            // When properties not name explicitly map using ForMember method  
+            public string FullName { get; set; }
+
+            // When properties name is same it is automatically map 
+            public string Mobile { get; set; }
+        }
+
+        public class UserDomain
+        {
+            public string Name { get; set; }
+            public string Mobile { get; set; }
+        }
+
+    }
+}
