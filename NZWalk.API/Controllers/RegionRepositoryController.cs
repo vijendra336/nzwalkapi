28a515a5b3f27cd2b072e62868eca5ed927aa16d
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-10-09 13:07:36 +0530

    35. Repository Pattern RegionRepositoryController.

---
+++ NZWalk.API/Controllers/RegionRepositoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalk.API.Models.DTO;
+using NZWalk.API.Repositories;
+
+//#3 Repository Pattern Implementation 
+
+namespace NZWalk.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionRepositoryController : ControllerBase
+    {
+        private readonly IRegionRepository regionRepository;
+
+        public RegionRepositoryController(IRegionRepository regionRepository)
+        {
+            this.regionRepository = regionRepository;
+        }
+
+        // GET ALL REGIONS
+        // GET: https://localhost:portnumber/api/regionsasync 
+        // Asynchronous Call GetAll
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+
+            // Get data from Database - Domain models 
+            // Use await to call which you want async 
+            var regionsDomain = await regionRepository.GetAllAsync();
+
+            // Map or converting this Domain models to DTOs 
+            var regionsDto = new List<RegionDto>();
+
+            foreach (var regionDomain in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+            }
+
+            // return DTOs to the client 
+            // exposing dto instead of domain model i.e. regionsDomain
+            return Ok(regionsDto);
+        }
+
+    }
+}
--- NZWalk.API/Program.cs
+++ NZWalk.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalk.API.Data;
+using NZWalk.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<NZWalkDbContexts>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString"))
     );
+
+builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
+
 
 var app = builder.Build();
 
