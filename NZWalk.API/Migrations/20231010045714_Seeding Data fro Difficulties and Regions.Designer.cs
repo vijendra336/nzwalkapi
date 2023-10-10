5d4f62da78f71b0c13fc5fd54938e1ff7c0bf9a1
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-10-10 10:42:53 +0530

    S5 C40 Seeding Diifficulty and Region using EF override OnModelCreating

--- NZWalk.API/Migrations/20231010045714_Seeding Data fro Difficulties and Regions.Designer.cs
+++ NZWalk.API/Migrations/20231010045714_Seeding Data fro Difficulties and Regions.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NZWalk.API.Data;
 
@@ -11,9 +12,11 @@ using NZWalk.API.Data;
 namespace NZWalk.API.Migrations
 {
     [DbContext(typeof(NZWalkDbContexts))]
-    partial class NZWalkDbContextsModelSnapshot : ModelSnapshot
+    [Migration("20231010045714_Seeding Data fro Difficulties and Regions")]
+    partial class SeedingDatafroDifficultiesandRegions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
