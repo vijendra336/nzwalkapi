4fc0dbdd397e3c8375f745d1df17e860743ec2a7
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-10-12 19:03:20 +0530

    S8 C60 Authentication - Seed Auth Data using IdentityRole - Reader and Writer roles.

--- NZWalk.API/Data/NZWalksAuthDbContext.cs
+++ NZWalk.API/Data/NZWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalk.API.Data
@@ -12,5 +13,32 @@ namespace NZWalk.API.Data
              * Fix this by making  DbContextOptions as generic one  -> DbContextOptions<NZWalksAuthDbContext> 
              * */
         }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "76c66479-a387-4b27-b1bd-7f49c6f36b51";
+            var writerRoleId = "751f81fd-c3ae-4c88-82b3-d0cd30ed4367";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name="Reader",
+                    NormalizedName="Reader".ToUpper(),
+                },
+                new IdentityRole {
+                    Id = writerRoleId,
+                    ConcurrencyStamp=writerRoleId,
+                    Name="Writer",
+                    NormalizedName ="Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+
     }
 }
