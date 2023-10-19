c51786e91fd569c8540a29900774460787922473
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-10-19 11:47:08 +0530

    S11 C85 MVC Consume WebAPI - GetAll method Region.

---
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models.DTO;
+
+namespace NZWalks.UI.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDto> response = new List<RegionDto>();
+            try
+            {
+                // Get all region from Web API 
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:7218/api/regionautomapper");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+                // get data in stringg 
+                //var stringResponseBody= await httpResponseMessage.Content.ReadAsStringAsync();
+                //ViewBag.Response = stringResponseBody;
+
+                // get data in json and map to model type RegionDto
+                response.AddRange( await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>() );
+
+            }
+            catch (Exception ex)
+            {
+                // Log the exception 
+                throw;
+            }
+           
+
+            return View(response);
+        }
+    }
+}
